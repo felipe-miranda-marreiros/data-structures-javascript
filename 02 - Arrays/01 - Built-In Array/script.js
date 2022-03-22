7c6e38856ec61385384d53472195f7be0be57c6b
felipe-miranda-marreiros <felipemirandahoje@gmail.com>
2022-03-22 09:49:16 -0300

    Built-in Array Methods - Dynamic Array

---
+++ 02 - Arrays/01 - Built-In Array/script.js
@@ -0,0 +1,22 @@
+//Métodos comuns
+
+/*
+
+Pop
+Unshift
+Shift
+Push
+Slice
+Reverse
+Sort
+etc
+ */
+
+const arrExemple = ["c", "a", "d", "e", "b"];
+
+arrExemple.push("f")
+arrExemple.unshift("g")
+arrExemple.splice(2, 0, "h")
+arrExemple.sort()
+
+console.log(arrExemple)
